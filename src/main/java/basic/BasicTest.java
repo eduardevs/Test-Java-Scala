de6c10a59762692c690960361f1e4719d3c1fd37
“eduardevs <jeseduardopi@gmail.com>
2024-02-16 20:46:09 +0100

    Make function power :
    * Check if number is not positive
    
    * Check if number is 0 returns 1
    
    * Check if the operation with the given value doesn't overflow
    
    * Catch errors and return NONE as Option

--- src/main/java/basic/BasicTest.java
+++ src/main/java/basic/BasicTest.java
@@ -7,11 +7,35 @@ import io.vavr.control.Option;
  */
 public class BasicTest {
 
-  /**
-   * return i ^ n for positive Integer, None otherwise
-   * alse return None in case of errors
-   */
-  public static Option<Integer> power(Integer i, Integer n) {
-    return Option.none();
-  }
+    /**
+     * return i ^ n for positive Integer, None otherwise
+     * alse return None in case of errors
+     */
+    public static Option<Integer> power(Integer i, Integer n) {
+        // Check if is not positive integer
+        if (i < 0 || n < 0) {
+            return Option.none();
+        }
+
+        try {
+            if (n == 0) {
+                return Option.of(1);
+            }
+
+            int value = i;
+            for (int j = 1; j < n; j++) {
+
+                // detecting overflow
+                if (Integer.MAX_VALUE / i < value) {
+                    return Option.none();
+                }
+                value *= i;
+            }
+
+            return Option.of(value);
+        } catch (ArithmeticException e) {
+            return Option.none();
+        }
+
+    }
 }
