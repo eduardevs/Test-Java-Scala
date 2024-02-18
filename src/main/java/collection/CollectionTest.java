70566a5c2a20daa42ea9678f25d6280ab8d0375a
“eduardevs <jeseduardopi@gmail.com>
2024-02-18 12:53:43 +0100

    Add compute2 functionality :
    * Implement strings methods
    
    * Duplicate the transformed string
    
    * Add in list result
    
    * Fix invalid string to add into result list

--- src/main/java/collection/CollectionTest.java
+++ src/main/java/collection/CollectionTest.java
@@ -1,5 +1,6 @@
 package collection;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,18 +11,39 @@ import java.util.List;
 class CollectionTest {
 
 
-  /**
-   * operation : x -> ((x * 2) + 3) ^ 5
-   */
-  public static List<Double> compute1(List<Integer> input) {
-    return null;
-  }
+    /**
+     * operation : x -> ((x * 2) + 3) ^ 5
+     */
+    public static List<Double> compute1(List<Integer> input) {
+        List<Double> results = new ArrayList<>();
 
-  /**
-   * operation : abc -> AbcAbc
-   */
-  public static List<String> compute2(List<String> input) {
-    return null;
-  }
+        for (Integer x : input) {
+            // Add to the result list
+            double result = Math.pow((x * 2) + 3, 5);
+            results.add(result);
+        }
 
+        return results;
+    }
+
+    /**
+     * operation : abc -> AbcAbc
+     */
+    public static List<String> compute2(List<String> input) {
+        List<String> results = new ArrayList<>();
+
+        for (String str : input) {
+            // Vérifier les chaines de caractères
+            if (str != null && !str.isEmpty()) {
+                String transformed = str.substring(0, 1).toUpperCase() + str.substring(1);
+                transformed += transformed; // Duplicate the transformed string
+                results.add(transformed); // Add to results
+            } else {
+                results.add(str);
+            }
+        }
+        return results;
+    }
 }
+
+
